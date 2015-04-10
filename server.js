fd5585314b48cd51a739275c6231fa05815a8766
Julien Derenty <jderenty@gmail.com>
2015-04-10 09:25:12 +0200

    Parse and save receiver + amount

--- server.js
+++ server.js
@@ -1,26 +1,30 @@
-var http = require ('http');            // For serving a basic web page.
-var mongoose = require ("mongoose");  // The reason for this demo.
+var http = require ('http');              // For serving a basic web page.
+var mongoose = require ("mongoose");      // The reason for this demo.
 var querystring = require('querystring');
+
+// The http server will listen to an appropriate port, or default to
+// port 5000.
+var port = process.env.PORT || 5000;
+
+
+
+
+/***************** DATABASE CONNEXION AND INITIALIZATION ****************/
 
 // Here we find an appropriate database to connect to, defaulting to
 // localhost if we don't find one.
-var uristring =
+var MongoURI =
 process.env.MONGOLAB_URI ||
 process.env.MONGOHQ_URL ||
 'mongodb://localhost/HelloMongoose';
 
-// The http server will listen to an appropriate port, or default to
-// port 5000.
-var theport = process.env.PORT || 5000;
-
-
 // Makes connection asynchronously.  Mongoose will queue up database
 // operations and release them when the connection is complete.
-mongoose.connect(uristring, function (err, res) {
+mongoose.connect(MongoURI, function (err, res) {
   if (err) {
-  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  console.log ('ERROR connecting to: ' + MongoURI + '. ' + err);
   } else {
-    console.log ('Succeeded connected to: ' + uristring);
+    console.log ('Succeeded connected to: ' + MongoURI);
   }
 });
 
@@ -31,6 +35,17 @@ var UserSchema = new mongoose.Schema({
 
 var User = mongoose.model('User', UserSchema);
 
+var OperationSchema = new mongoose.Schema({
+  slack_id_sender: { type: String },
+  slack_id_receiver: { type: String },
+  amount: { type: Number }
+});
+
+var Operation = mongoose.model('Operation', OperationSchema);
+
+
+
+/***************** APPLICATION'S LOGIC ****************/
 
 http.createServer(function (req, res) {
     var fullBody = '';
@@ -58,13 +73,26 @@ http.createServer(function (req, res) {
           res.end( JSON.stringify(responseBody) );
 
         }else{
-          findTarget(decodedBody.text, function(user){
+          findTarget(decodedBody.text, function(target){
             var amount = findAmount(decodedBody.text);
-            responseBody = { text : 'Donné ' + amount + ' à ' +user.name };
 
-            res.writeHead(200, {'Content-Type': 'application/json'});
-            res.end( JSON.stringify(responseBody) );
-        
+            amount = parseInt(amount);
+
+            var op = new Operation({ slack_id_sender: user.slack_id, slack_id_receiver: target.slack_id, amount: amount  });
+            op.save();
+
+              processBank(user, function(user_bank){
+                processBank(target, function(target_bank){
+
+                  responseBody = { text : 'Donné ' + amount + ' à ' +target.name + '\n'+ user.name +':' + user_bank+ '\n'+ target.name+':' + target_bank };
+
+                  res.writeHead(200, {'Content-Type': 'application/json'});
+                  res.end( JSON.stringify(responseBody) );
+              
+              });
+
+            });
+
           });
         }
 
@@ -72,15 +100,13 @@ http.createServer(function (req, res) {
 
     });
 
-    new User({ slack_id: 'U02BFK30A', name: 'bertran' }).save();
 
-}).listen(theport, '127.0.0.1');
+}).listen(port, '127.0.0.1');
 
-console.log('Server running at http://127.0.0.1:8080/');
+console.log('Server running at http://127.0.0.1:'+port);
 
 var findTarget = function(text, callback){
   var target_id = text.match(/\<@(.*)\>/).pop();
-
     User.find({ slack_id: target_id}, function(err, users){
     callback(users[0]);
   });
@@ -90,6 +116,19 @@ var findTarget = function(text, callback){
 var findAmount = function(text){
   var numbers = text.match(/\d+/);
   return numbers[0];
+}
+
+var processBank = function(user, callback){
+  Operation.aggregate({ $group: { _id:{ slack_id_sender: user.slack_id } , total: {$sum: "$amount"} } } 
+   , function(err, senders){
+        console.log(senders);
+    Operation.aggregate({ $group: { _id:{ slack_id_receiver: user.slack_id } , total: {$sum: "$amount"} } } 
+       , function(err, receivers){
+        console.log(receivers);
+        callback(receivers[0].total - senders[0].total);
+    });
+  });
+
 }
 
 
