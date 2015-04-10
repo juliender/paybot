00fd97144abdce07b001b12d890e95fa7c57a953
Julien Derenty <jderenty@gmail.com>
2015-04-10 11:43:32 +0200

    Fix

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ http.createServer(function (req, res) {
               res.writeHead(200, {'Content-Type': 'application/json'});
               res.end( JSON.stringify(responseBody) );
               return;
-            }
+            });
           }
 
           // First find for who is the money
