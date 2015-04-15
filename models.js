626e462589cabf704c38e26e3b5edf469cc3aa5a
Julien Derenty <jderenty@gmail.com>
2015-04-15 12:18:55 +0200

    Fix model

--- models.js
+++ models.js
@@ -71,7 +71,7 @@ module.exports = {
 
 
   // Create and save the transaction in database
-  newOperation : function(slack_id_sender, slack_id_receiver, amount, callback){
+  newOperation : function(slack_id_sender_, slack_id_receiver_, amount_, callback){
         var op = new Operation({ slack_id_sender: slack_id_sender_, slack_id_receiver: slack_id_receiver_, amount: amount_  });
         op.save(callback);
   },
